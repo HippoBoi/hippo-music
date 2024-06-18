60bb1c6107316899f41814d9b37c04cef777438f
HippoBoi <tagrifavc@gmail.com>
2024-06-18 17:39:59 -0400

    added progress bar and time

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,5 +1,7 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Song } from './SongsList';
+import playButton from "../assets/images/playbutton.png";
+import pauseButton from "../assets/images/pausebutton.png";
 import "./AudioPlayer.css";
 
 interface Props {
@@ -9,32 +11,84 @@ interface Props {
 const AudioPlayer = ({ song }: Props) => {
     const audioRef = useRef<HTMLAudioElement>(null);
     const [isPlaying, setPlaying] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [songTime, setSongTime] = useState(0);
+    const [songLength, setSongLength] = useState(0);
+    const [buttonImg, setButtonImg] = useState(playButton);
 
     const togglePlay = () => {
         if (audioRef.current) {
             if (audioRef.current.paused) {
                 audioRef.current.play();
                 setPlaying(true);
+                setButtonImg(pauseButton);
             } 
             else {
                 audioRef.current.pause();
                 setPlaying(false);
+                setButtonImg(playButton);
             }
         }
     }
 
+    const updateProgress = () => {
+        if (audioRef.current) {
+            setProgress((audioRef.current.currentTime / audioRef.current.duration) * 100);
+            setSongTime(audioRef.current.currentTime);
+            setSongLength(audioRef.current.duration);
+        }
+    };
+    const onSongEnd = () => {
+        setPlaying(false);
+        setButtonImg(playButton);
+    }
+
+    const formatTime = (time: number) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return(`0${minutes}:${seconds < 10 ? "0" : ""}${seconds}`);
+    }
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.addEventListener("timeupdate", updateProgress);
+            audioRef.current.addEventListener("ended", onSongEnd);
+
+            return () => {
+                audioRef.current?.removeEventListener("timeupdate", updateProgress);
+                audioRef.current?.removeEventListener("ended", onSongEnd);
+            };
+        }
+    }, []);
+
     return (
-        <div className={"audio-player"}>
+        <div className="audio-player">
             <h2>{song.name}</h2>
+
             <audio ref={audioRef}>
                 <source src={song.URL} type="audio/mp3" />
                 No se pudo reproducir audio en este navegador.
             </audio>
-            <button 
-                className={"audio-player-button"}
+
+            <button
                 onClick={togglePlay}>
-                {isPlaying ? "Pausa" : "Reproducir"}
+                <img 
+                    className={isPlaying ? "pause-image" : "play-image"} 
+                    src={buttonImg} />
             </button>
+
+            <div className='progress-bar-container'>
+                <div 
+                    className="progress-bar"
+                    style={{ width: `${progress}%` }}>
+                </div>
+            </div>
+
+            <div className='time-container'>
+                <span>
+                    {formatTime(songTime) + " / " + formatTime(songLength)}
+                </span>
+            </div>
         </div>
     );
 }
