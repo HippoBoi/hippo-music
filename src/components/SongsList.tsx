2bb432266a2a153bc58f521b6aaab554e096342c
HippoBoi <tagrifavc@gmail.com>
2024-06-19 00:53:44 -0400

    search song bar

--- src/components/SongsList.tsx
+++ src/components/SongsList.tsx
@@ -3,10 +3,12 @@ import astray from "../assets/audios/astrayinst.mp3";
 import spacecart from "../assets/audios/spacecart.mp3";
 import endstats from "../assets/audios/endstats.mp3";
 import AudioPlayer from "./AudioPlayer";
+import SearchBar from "./SearchBar";
 
 export interface Song {
     id: number;
     name: string;
+    author: string;
     description: string;
     URL: string;
 }
@@ -15,16 +17,19 @@ const SongsList = () => {
     const songs: Song[] = [{
             id: 0,
             name: "Astray (Instrumental)", 
+            author: "HippoBoi",
             description: "aaaa", 
             URL: astray
         }, {
             id: 1,
             name: "Space Cart",
+            author: "HippoBoi",
             description: "cccc",
             URL: spacecart
         }, {
             id: 2,
             name: "End Stats",
+            author: "HippoBoi",
             description: "bbbb",
             URL: endstats
         }
@@ -32,6 +37,7 @@ const SongsList = () => {
 
     return (
         <div className="list">
+            <SearchBar />
             {songs.map((song) => (
                 <div className="listItem" key={song.id}>
                     <AudioPlayer song={song} />
